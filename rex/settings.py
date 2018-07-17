3956680155c8d96313d5bc07dd68cd4017d14ab7
hotsiam <zanemassey@hotmail.com>
2018-07-17 12:34:17 +1000

    road map and static

--- rex/settings.py
+++ rex/settings.py
@@ -127,11 +127,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'rex/static/')
 ]
 
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
